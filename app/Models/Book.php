b145d19402de2ad860ad6bc8049e386422464a09
ALBaraa <albaraawork3@gmail.com>
2025-01-09 11:27:45 +0200

    Add filtering options for popular and highest rated books in BookController and update index view

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -42,7 +42,6 @@ class Book extends Model
         return $query->having('reviews_count', '>=', $minReviews);
     }
 
-
     private function DateRangeFilter(Builder $query, $from = null, $to = null)
     {
         if ($from && !$to) {
@@ -53,5 +52,29 @@ class Book extends Model
             $query->whereBetween('created_at', [$from, $to]);
         }
 
+    }
+
+    public function scopePopularLastMonth(Builder $query): Builder|QueryBuilder
+    {
+        return $query->popular(now()->subMonth(), now())
+            ->highestRated(now()->subMonth(), now());
+    }
+
+    public function scopePopularLast6Months(Builder $query): Builder|QueryBuilder
+    {
+        return $query->popular(now()->subMonths(6), now())
+            ->highestRated(now()->subMonths(6), now());
+    }
+
+    public function scopeHighestRatedLastMonth(Builder $query): Builder|QueryBuilder
+    {
+        return $query->highestRated(now()->subMonth(), now())
+            ->popular(now()->subMonth(), now());
+    }
+
+    public function scopeHighestRatedLast6Months(Builder $query): Builder|QueryBuilder
+    {
+        return $query->highestRated(now()->subMonths(6), now())
+            ->popular(now()->subMonths(6), now());
     }
 }
